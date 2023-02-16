fada466d6c0f118a81539986712435d0c7dd12da
daniel-gomm <daniel.gomm@outlook.de>
2023-02-16 09:43:50 +0100

    update .gitignore

--- code/etl_service/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# create ssl key pair
-openssl req -x509 -nodes -newkey rsa:2048 -keyout ../../deploy/docker/etl_service/key.pem -out ../../deploy/docker/etl_service/cert.pem -sha256 -days 365 \
-    -subj "/CN=localhost"
-
-#build docker image
-docker build . -t etl_service
